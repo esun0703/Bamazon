acc99778f64b58402034a83af3e9ffd1d911e65b
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-02-24 20:46:27 -0500

    added .gitignore, bamazonCustomer.js: added function showAll, and function newPurchase wiht inquirer prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,5 +12,29 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
 	if(err) throw err;
 	console.log("connected as id " + connection.threadID);
-	newPrompt();
+	showAll();
 });
+
+//function that shows all products available
+function showAll(){
+	connection.query('SELECT id, product_name, price FROM products',function(err,rows){
+		console.log(rows)
+	});
+}
+
+//function that prompts with two messages: ID of product liked to purchase, how many units
+function newPurchasePrompt(){
+	inquirer.prompt(
+		{
+			name: "productID",
+			type:"input",
+			message: "What is the ID of the product you wish to purchase?"
+		},
+		{
+			name: "numberOfUnits",
+			type:"input",
+			message: "How many units would you like to purchase?"
+		},
+
+	)
+}
