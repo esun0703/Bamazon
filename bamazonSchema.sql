509f93fde28c95e9673f8c1cd8dad73a5b07010f
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-02-24 20:27:07 -0500

    added bamazonCustomer.js: added var mysql, var inquire, var connection and connection.connect

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -6,8 +6,20 @@ CREATE TABLE products (
 	id INT NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(100) NOT NULL,
 	department_name VARCHAR(100),
-	price DECIMAL(100,2) NOT NULL,
+	price DECIMAL(60,2) NOT NULL,
 	stock_quantity INT NOT NULL,
 	PRIMARY KEY (id)
-
 );
+
+INSERT INTO products (product_name, price, stock_quantity)
+VALUES 
+	("Hairties", 2.00, 100), 
+	("Macbook", 2000.00, 50), 
+	("Himalayan Salt Lamp", 15.00, 200),
+	("Bungee Chair", 26.00, 90),
+	("Battlefield 1 Game", 45.00, 130),
+	("Coffee Mug", 5.00, 600),
+	("My Little Pony Applejack Plush Toy", 27.00, 5),
+	("XBox 1", 300.00, 65),
+	("Green Tea Face Wash", 17.00, 30),
+	("Coffee Maker", 25.00, 270);
